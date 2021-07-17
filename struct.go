fdd3a73c504b0db0ae5c3764e66657dd81e88695
Bailey <bsy2658@gmail.com>
2021-07-17 19:02:46 +0900

    자료구조 추가

---
+++ struct.go
@@ -0,0 +1,13 @@
+package main
+
+type data struct {
+	ID         int
+	author     string
+	createtime string
+	edittime   string
+	title      string
+	tags       []string
+	hide       bool
+	version    int
+	category   string
+}
