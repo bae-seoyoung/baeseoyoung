5ac53d7f08b7451af0ada1b98ef61ed09ce1c7c7
woong <woong@lazypic.org>
2021-10-18 20:34:59 +0900

    additem을 추가함.

--- dbapi.go
+++ dbapi.go
@@ -9,6 +9,18 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+// AddItem 은 데이터베이스에 Item(Project, Card, Client)를 넣는다.
+func AddItem(client *mongo.Client, i Item) error {
+	collection := client.Database(*flagDBName).Collection("item")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_, err := collection.InsertOne(ctx, i)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // RmItem 는 id를 받아서, 해당 컬렉션에서 id가 일치하는 Item을 삭제한다.
 func RmItem(client *mongo.Client, id string) error {
 	collection := client.Database(*flagDBName).Collection("item")
